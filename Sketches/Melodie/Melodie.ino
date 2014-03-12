a8de6d3bfef02fa73334727ad39ca0ce24ff5b5e
Stefan Teitge <stefan.teitge@gmail.com>
2014-03-12 19:44:29 +0100

    Melodiebeispiel

---
+++ Sketches/Melodie/Melodie.ino
@@ -0,0 +1,50 @@
+/**
+ * Basiert auf Arduino Originaltutorial "Melody".
+ */
+
+int speakerPin = 9;
+
+int length = 15; // Anzahl der Noten
+char notes[] = "ccggaagffeeddc "; // Leerzeichen repräsentiert die Pause
+int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
+
+int tempo = 50;
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+
+  // spiele die Note ab
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+
+void setup() {
+  pinMode(speakerPin, OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') {
+      delay(beats[i] * tempo); // Pause
+    } else {
+      playNote(notes[i], beats[i] * tempo);
+    }
+
+    // Pause zwischen den Noten
+    delay(tempo / 2); 
+  }
+}
+
